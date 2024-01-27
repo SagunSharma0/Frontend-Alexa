3e315aa546eb064bc44e7db4b6f9ee307d22038e
SagunSharma0 <94883818+SagunSharma0@users.noreply.github.com>
2024-01-28 00:45:15 +0545

    action created

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,8 @@
+import Link from "next/link";
 
+import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
+
 
 const Navbar =  () => {
 
@@ -8,7 +11,11 @@ const Navbar =  () => {
     <div className="border-b">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-            Navbar!
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">Alexa Store</p>
+          </Link>
+          <MainNav data={[]} />
+          
         </div>
       </Container>
     </div>
