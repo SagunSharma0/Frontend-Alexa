13286116846d06fe5060bbf8c61ee55f14a22b9a
SagunSharma0 <94883818+SagunSharma0@users.noreply.github.com>
2024-01-28 00:29:02 +0545

    main nav created

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,16 @@
 
-const Navbar = () => {
+import Container from "@/components/ui/container";
+
+const Navbar =  () => {
+
 
   return ( 
     <div className="border-b">
-        Navbar
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+            Navbar!
+        </div>
+      </Container>
     </div>
   );
 };
